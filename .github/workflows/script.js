8f31ffb211554fd462dbdb854ccccd9d5c56b9fe
cai0dada <125868317+cai0dada@users.noreply.github.com>
2024-01-04 07:10:18 +0800

    Add files via upload

---
+++ .github/workflows/script.js
@@ -0,0 +1,14 @@
+/*!
+Pixelated Background
+Copyright (c) 2023 by Wakana Y.K. (https://codepen.io/wakana-k/pen/GRegEgq)
+*/
+"use strict";
+console.clear();
+
+var root = document.querySelector(":root");
+function spotlight(e) {
+  root.style.setProperty("--x", e.pageX + "px");
+  root.style.setProperty("--y", e.pageY + "px");
+}
+window.addEventListener("pointermove", spotlight);
+window.addEventListener("pointerdown", spotlight);
